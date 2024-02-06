648c02783b8bcd94c7c45cdb51a4760246959db6
Axel Meyer <118254328+axel-meyer-fnt@users.noreply.github.com>
2024-02-06 15:57:39 +0100

    fix(commitlint): extend max length of commit messages (#4)

--- cmd-commitlint.config.js
+++ cmd-commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   helpUrl: 'https://fntgrp.sharepoint.com/sites/DevelopmentOperations/SitePages/GIT.aspx',
   rules: {
-    'header-max-length': [2, 'always', 150],
+    'header-max-length': [2, 'always', 250],
     'type-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
     'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'ui']],
@@ -11,6 +11,6 @@ module.exports = {
     'subject-case': [0], // don't check the subject's case
     'subject-empty': [2, 'never'],
     'body-leading-blank': [2, 'always'],
-    'body-max-line-length': [2, 'always', 150]
+    'body-max-line-length': [2, 'always', 250]
   },
 };
